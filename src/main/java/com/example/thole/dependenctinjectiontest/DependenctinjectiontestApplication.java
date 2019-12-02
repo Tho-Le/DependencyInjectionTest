c054f106e418b16eae401f7d2bc32e3db8e88b70
Tho-Le <doctorvician@gmail.com>
2019-12-01 22:35:19 -0600

    Initialize project to test dependency injections

---
+++ src/main/java/com/example/thole/dependenctinjectiontest/DependenctinjectiontestApplication.java
@@ -0,0 +1,13 @@
+package com.example.thole.dependenctinjectiontest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DependenctinjectiontestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DependenctinjectiontestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/thole/dependenctinjectiontest/DependenctinjectiontestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thole.dependenctinjectiontest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DependenctinjectiontestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
